a98446ff756b433c825b13d76c20b24444a9484c
Shameem A R <mobihack.me@gmail.com>
2024-07-03 20:35:53 +0530

    fix(client); better legibility in dark mode

--- client/src/components/Textarea/Textarea.tsx
+++ client/src/components/Textarea/Textarea.tsx
@@ -1,4 +1,4 @@
-import { forwardRef,TextareaHTMLAttributes } from "react";
+import { forwardRef, TextareaHTMLAttributes } from "react";
 
 import { cn } from "@/lib/utils";
 
@@ -12,7 +12,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-600 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-brand-900 dark:placeholder:text-slate-400 dark:focus-visible:ring-brand-300",
+          "flex min-h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-600 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:text-gray-300 dark:bg-slate-950 dark:ring-offset-brand-900 dark:placeholder:text-slate-500 dark:focus-visible:ring-brand-300",
           error && "border-red-500",
           className
         )}
